034811cbcc5cc017d15deebbaa0ad5f2cd1e381c
myuber <ogari.gari.za@gmail.com>
2020-07-21 08:19:13 +0900

    ジャンケンアプリを作成しました

--- MyJanken/MyJanken/ViewController.swift
+++ MyJanken/MyJanken/ViewController.swift
@@ -19,7 +19,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answerLabel: UILabel!
     
+    var answerNumber = 0
     @IBAction func shuffleAction(_ sender: Any) {
+        var newAnswerNumber = 0
+        
+        repeat {
+            newAnswerNumber = Int.random(in:0..<3)
+            // 前回と異なる結果の場合はrepeatを抜ける
+        } while answerNumber == newAnswerNumber
+        
+        answerNumber = newAnswerNumber
+        
+        if answerNumber == 0 {
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named:"gu")
+        } else if answerNumber == 1 {
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named:"choki")
+        } else if answerNumber == 2 {
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named:"pa")
+        }
+        
+        
     }
+    
+    
+    
 }
 
