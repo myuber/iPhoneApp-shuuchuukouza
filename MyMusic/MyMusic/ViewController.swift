9b94ef3fb6bdab0357edf788500a0d1d7cc61c4f
myuber <ogari.gari.za@gmail.com>
2020-07-22 08:26:51 +0900

    音楽アプリのUIを作りました

---
+++ MyMusic/MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by がり on 2020/07/22.
+//  Copyright © 2020 がり. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
