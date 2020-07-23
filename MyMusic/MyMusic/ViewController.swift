9fcaf2f58703fa5380543ed3c5f1d6e9d6e8d5ce
myuber <ogari.gari.za@gmail.com>
2020-07-23 21:44:39 +0900

    音楽アプリが完成しました

--- MyMusic/MyMusic/ViewController.swift
+++ MyMusic/MyMusic/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,43 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let cymbalPath = Bundle.main.bundleURL.appendingPathComponent("cymbal.mp3")
+    var cymbalPlayer = AVAudioPlayer()
+    
+    let guitarPath = Bundle.main.bundleURL.appendingPathComponent("guitar.mp3")
+    var guitarPlayer = AVAudioPlayer()
+    
+    let backmusicPath = Bundle.main.bundleURL.appendingPathComponent("backmusic.mp3")
+    var backmusicPlayer = AVAudioPlayer()
+    
+    
+    @IBAction func cymbal(_ sender: Any) {
+        soundPlayer(player: &cymbalPlayer, path: cymbalPath, count: 0)
+    }
+    
+    @IBAction func guitar(_ sender: Any) {
+        soundPlayer(player: &guitarPlayer, path: guitarPath, count: 0)
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        soundPlayer(player: &backmusicPlayer, path: backmusicPath, count: -1)
+    }
 
-
+    @IBAction func stop(_ sender: Any) {
+        backmusicPlayer.stop()
+    }
+    
+    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int){
+        do {
+            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+            player.numberOfLoops = count
+            player.play()
+        } catch {
+            print("エラーが発生しました!")
+        }
+        
+    }
+  
 }
 
