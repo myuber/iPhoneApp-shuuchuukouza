eca27d837bfeb2265dd9e04977eac679142e3b3b
myuber <ogari.gari.za@gmail.com>
2020-07-26 17:53:37 +0900

    タイマーアプリ作成中。SettingViewControllerを作成しました

---
+++ MyTimer/MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by がり on 2020/07/26.
+//  Copyright © 2020 がり. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
