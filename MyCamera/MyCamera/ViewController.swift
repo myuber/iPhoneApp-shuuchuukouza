05e62c3845932dc05b6ec92580498eb1a98cbe39
myuber <ogari.gari.za@gmail.com>
2020-08-02 23:34:44 +0900

    カメラで撮影した写真をSNSにできるようにしました

--- MyCamera/MyCamera/ViewController.swift
+++ MyCamera/MyCamera/ViewController.swift
@@ -17,10 +17,45 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
 
 
     @IBOutlet weak var pictureImage: UIImageView!
+    // カメラを起動するをタップすると起動
     @IBAction func cameraButtonAction(_ sender: Any) {
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("カメラは利用できます")
+            // (1)UIImagePickerControllerのインスタンスを作成
+            let imagePickerController = UIImagePickerController()
+            // (2)sourceTypeにcameraを設定
+            imagePickerController.sourceType = .camera
+            // (3)dalegate設定
+            imagePickerController.delegate = self
+            // (4)モーダルビューで表示
+            present(imagePickerController, animated: true, completion: nil)
+        }else{
+            print("カメラは利用できません")
+        }
         
     }
+    // SNSに投稿するをタップすると実行
     @IBAction func shareButtonAction(_ sender: Any) {
+        // 表示画像をアンラップしてシェア画像を取り出す
+        if let shareImage = pictureImage.image {
+            // UIActivityViewControllerに渡す配列を作成
+            let shareItems = [shareImage]
+            // UIActivityViewControllerにシェア画像を渡す
+            let controller = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            // iPadで落ちてしまう対策
+            controller.popoverPresentationController?.sourceView = view
+            // UIActivityViewControllerを表示
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    // (1)撮影が終わった時に呼ばれるdelegateメソッド
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        // (2)撮影した画像を配置したpictureImageに渡す
+        pictureImage.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        // (3)モーダルビューを閉じる
+        dismiss(animated: true, completion: nil)
     }
 }
 
